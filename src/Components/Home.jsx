53403a0bde0c2ac809dfe524abe945184a856ad5
Rajesh <rajeshranjan8271@gmail.com>
2024-09-03 08:51:01 +0530

    test

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import startGame from "../assets/startGame.jpg";
+import startGame from "/assets/startGame.jpg";
 import { Link } from "react-router-dom";
 
 const Home = () => {
